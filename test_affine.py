11caf48ef19aaeff9c97a97c893b16ddc0aca193
Yu Sun <Yu.Sun@petermac.org>
2023-07-14 12:19:10 +1000

    Verification for the affine grid and grid sample

---
+++ test_affine.py
@@ -0,0 +1,29 @@
+# To confirm how the affine grid works
+
+import torch
+import torch.nn.functional as F
+
+z = 6
+img = torch.zeros((1,1,3,3,z)).float()
+img[0,0,2,2,2] = 1
+
+affine_matrix = torch.tensor([
+    [1.0, 0.1, 0, 0.4],
+    [0, 1, 0, 0.1],
+    [0, 0, 1, 0.2]
+]).float().unsqueeze(0)
+
+grid = F.affine_grid(affine_matrix, (1,1,z,3,3), align_corners=True)
+
+warped = F.grid_sample(img.permute(0,1,4,3,2), grid, align_corners=True).permute(0,1,4,3,2)
+
+print(warped)
+
+from warp_mesh import get_3d_com, cvt_aff_matrix
+
+print(get_3d_com(warped[0])-1/2)
+
+
+matrix = cvt_aff_matrix(affine_matrix, (3,3,z))
+matmul_result = torch.matmul(matrix[0], torch.tensor([2.0, 2, 2, 1]))
+print(f'matmul: {matmul_result}')
