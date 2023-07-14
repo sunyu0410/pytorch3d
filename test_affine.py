1fba6cf4def308b71672e1522c2897b8a4c06f05
Yu Sun <Yu.Sun@petermac.org>
2023-07-14 14:40:50 +1000

    Assert translations

--- test_affine.py
+++ test_affine.py
@@ -8,7 +8,7 @@ img = torch.zeros((1,1,3,3,z)).float()
 img[0,0,2,2,2] = 1
 
 affine_matrix = torch.tensor([
-    [1.0, 0.1, 0, 0.4],
+    [1.0, 0, 0, 0.4],
     [0, 1, 0, 0.1],
     [0, 0, 1, 0.2]
 ]).float().unsqueeze(0)
@@ -20,10 +20,12 @@ warped = F.grid_sample(img.permute(0,1,4,3,2), grid, align_corners=True).permute
 print(warped)
 
 from warp_mesh import get_3d_com, cvt_aff_matrix
-
-print(get_3d_com(warped[0])-1/2)
+com = get_3d_com(warped[0])-1/2
+print(com)
 
 
 matrix = cvt_aff_matrix(affine_matrix, (3,3,z))
 matmul_result = torch.matmul(matrix[0], torch.tensor([2.0, 2, 2, 1]))
 print(f'matmul: {matmul_result}')
+
+torch.testing.assert_close(com, matmul_result[:-1])
